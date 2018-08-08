2f6e1fe2c7257daace9307669394381e4395edd1
sugawara <gomisuteru@gmail.com>
2018-08-08 16:23:34 +0900

    stage.witdhプロパティを参照するstage.widthProperty()を追加する

---
+++ HelloJavaFX.java
@@ -0,0 +1,31 @@
+// HelloJavaFX.java
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class HelloJavaFX extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        stage.setTitle("Hello, JavaFX");
+        stage.setWidth(540);
+        stage.setHeight(220);
+
+		//stage.widthプロパティの値を参照する
+		System.out.println(stage.widthProperty());
+		// ReadOnlyDoubleProperty [bean: javafx.stage.Stage@4d4c7159, name: width, value: 540.0]
+
+
+        Label lblMsg = new Label("Hello, JavaFX");
+        lblMsg.setAlignment(Pos.CENTER);
+        lblMsg.setFont(new Font(64));
+
+        stage.setScene(new Scene(lblMsg));
+        stage.show();
+    }
+}
